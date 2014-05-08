bdb0637ac450d3bd2c145696eb06799d7d1c96cb
MDiana <diana.malabard@gmail.com>
2014-05-08 23:41:26 +0200

    avancement des tests

--- Src/scriptTestPerf.py
+++ Src/scriptTestPerf.py
@@ -12,8 +12,11 @@ import sys
 	
 # Fonctions définissant l'exécution pour chaque type de fichier (pour les Timer)
 
-def xml(id) :
-	T=xmlParser("XML_tests/%d.xml" % id)
+def xml(num) :
+	if num<=1500:
+		T=xmlParser("XML_tests/treeXml1To1500/tree%d.xml" % num)
+	elif num<=1900:
+		T=xmlParser("XML_tests/treeXml1501To1900/tree%d.xml" % num)
 def dot() :
 	T=dotParser("XML_tests/test.dot")
 def strp() :
@@ -25,14 +28,25 @@ def coord() :
 def toasy() :
 	toAsymptote(T)
 
-def tonx() :
+def tonxpng() :
 	toNetworkX(T)
+
+def tonxpdf() :
+	toNetworkX(T, outputformat="pdf")
+
+def tonxeps() :
+	toNetworkX(T, outputformat="eps")
+
+def tonxsvg() :
+	toNetworkX(T, outputformat="svg")
 
 def totikz() :
 	toTikz(T)
 
 nbNode=[]
 nbNode.append(0)
+nbNodeNX=[]
+nbNodeNX.append(0)
 execTimeXML=[]
 execTimeXML.append(0)
 execTimeDOT=[]
@@ -47,53 +61,83 @@ execTimetoTikZ=[]
 execTimetoTikZ.append(0)
 execTimetoAsy=[]
 execTimetoAsy.append(0)
-execTimetoNX=[]
-execTimetoNX.append(0)
+execTimetoNXpng=[]
+execTimetoNXpng.append(0)
+execTimetoNXpdf=[]
+execTimetoNXpdf.append(0)
+execTimetoNXeps=[]
+execTimetoNXeps.append(0)
+execTimetoNXsvg=[]
+execTimetoNXsvg.append(0)
 
 nbIte=10
 i=1
 
-while i<=3 :
+while i<=50 :
 	# Premier parsing pour récupérer la structure d'arbre
 	# pour déterminer le nombre de noeuds contenu
-	T=xmlParser("XML_tests/%d.xml" % i)
-	nbNode.append(T.nbNode())
+	if i<=1500:
+		T=xmlParser("XML_tests/treeXml1To1500/tree%d.xml" % i)
+	elif i<=1900:
+		T=xmlParser("XML_tests/treeXml1501To1900/tree%d.xml" % i)
+	n=T.nbNode()
+	#nbNode.append(n)
+	# Chercher le slot où placer n
+	slot=0
+	while slot<len(nbNode) and nbNode[slot]<n:
+		slot+=1
+		#if nbNode[slot]!=n:
+		nbNode.insert(slot, n)
+	slotNX=0
+	if n<=5:
+		#nbNodeNX.append(n)
+		while slotNX<len(nbNodeNX) and nbNodeNX[slotNX]<n:
+			slotNX+=1
+		#if slotNX==len(nbNodeNX) or [slotNX]!=n:
+		nbNodeNX.insert(slotNX, n)
 	
 	print ('Parsing de l\'arbre %d...' % i)
 	
 	# Création du Timer pour le parsing XML
 	test = timeit.Timer("xml(%d)"%i, "from __main__ import xml")
-	execTimeXML.append(test.timeit(nbIte)/nbIte)
+	execTimeXML.insert(slot, test.timeit(nbIte)/nbIte)
 	
 	# Création du Timer pour le parsing STR
 	T.toStrFile("XML_tests/test")
 	test = timeit.Timer("strp()", "from __main__ import strp")
-	execTimeSTR.append(test.timeit(nbIte)/nbIte)
+	execTimeSTR.insert(slot, test.timeit(nbIte)/nbIte)
 	
-	## Création du Timer pour le parsing DOT
-	#T.toDotFile("XML_tests/test")
-	#test = timeit.Timer("dot()", "from __main__ import dot")
-	#execTimeDOT.append(test.timeit(nbIte)/nbIte)
+	# Création du Timer pour le parsing DOT
+	T.toDotFile("XML_tests/test")
+	test = timeit.Timer("dot()", "from __main__ import dot")
+	execTimeDOT.insert(slot, test.timeit(nbIte)/nbIte)
 	
 	print ('ComputeCoord pour l\'arbre %d...' % i)
 	
 	# Création du Timer pour computeCoord
 	test = timeit.Timer("coord()", "from __main__ import coord")
-	execTimeCoord.append(test.timeit(nbIte)/nbIte)
+	execTimeCoord.insert(slot, test.timeit(nbIte)/nbIte)
 	
 	print ('Génération de la sortie de l\'arbre %d...' % i)
 	
 	# Création du Timer pour TIKZ
 	test = timeit.Timer("totikz()", "from __main__ import totikz")
-	execTimetoTikZ.append(test.timeit(nbIte)/nbIte)
+	execTimetoTikZ.insert(slot, test.timeit(nbIte)/nbIte)
 	
 	# Création du Timer pour ASY
 	test = timeit.Timer("toasy()", "from __main__ import toasy")
-	execTimetoAsy.append(test.timeit(nbIte)/nbIte)
+	execTimetoAsy.insert(slot, test.timeit(nbIte)/nbIte)
 	
-	# Création du Timer pour NX
-	test = timeit.Timer("tonx()", "from __main__ import tonx")
-	execTimetoNX.append(test.timeit(nbIte)/nbIte)
+	if n<=5:
+		# Création du Timer pour NX
+		test = timeit.Timer("tonxpng()", "from __main__ import tonxpng")
+		execTimetoNXpng.insert(slotNX, test.timeit(nbIte)/nbIte)
+		test = timeit.Timer("tonxpdf()", "from __main__ import tonxpdf")
+		execTimetoNXpdf.insert(slotNX, test.timeit(nbIte)/nbIte)
+		test = timeit.Timer("tonxeps()", "from __main__ import tonxeps")
+		execTimetoNXeps.insert(slotNX, test.timeit(nbIte)/nbIte)
+		test = timeit.Timer("tonxsvg()", "from __main__ import tonxsvg")
+		execTimetoNXsvg.insert(slotNX, test.timeit(nbIte)/nbIte)
 	
 	i+=1
 
@@ -102,27 +146,41 @@ while i<=3 :
 
 plt.figure(2)
 plt.plot(nbNode, execTimeXML, 'b')
-#plt.plot(nbNode, execTimeDOT, 'r')
+plt.plot(nbNode, execTimeDOT, 'r')
 plt.plot(nbNode, execTimeSTR, 'g')
 plt.xlabel('Nombre de noeuds')
 plt.ylabel('Temps d\'exécution (ms)')
 plt.title('Temps d\'exécution des parser \n en fonction du nombre de noeuds de l\'arbre à parser')
-plt.legend(("Parser XML", "Parser STR"), 'best')#, "Parser DOT"
-plt.show()
+plt.legend(("Parser XML", "Parser DOT", "Parser STR"), 'best')
+plt.savefig("execTimeParsers.png")
 
 plt.figure(3)
 plt.plot(nbNode, execTimetoAsy, 'b')
-plt.plot(nbNode, execTimetoNX, 'r')
 plt.plot(nbNode, execTimetoTikZ, 'g')
 plt.xlabel('Nombre de noeuds')
 plt.ylabel('Temps d\'exécution (ms)')
-plt.title('Temps d\'exécution des générateurs \n en fonction du nombre de noeuds de l\'arbre à afficher')
-plt.legend(("Asymptote", "NetworkX", "TikZ"), 'best')
-plt.show()
+plt.title('Temps d\'exécution des générateurs Asymptote et TikZ \n'+
+		  ' en fonction du nombre de noeuds de l\'arbre à afficher')
+plt.legend(("Asymptote", "TikZ"), 'best')
+plt.savefig("execTimeGenerators.png")
 
 plt.figure(4)
+plt.plot(nbNodeNX, execTimetoNXpng, 'r')
+plt.plot(nbNodeNX, execTimetoNXpdf, 'g')
+plt.plot(nbNodeNX, execTimetoNXeps, 'b')
+plt.plot(nbNodeNX, execTimetoNXsvg, 'm')
+plt.xlabel('Nombre de noeuds')
+plt.ylabel('Temps d\'exécution (ms)')
+plt.title('Temps d\'exécution du générateur NetworkX \n '+
+		  'en fonction du nombre de noeuds de l\'arbre à afficher \n'+
+		  ' et du type de sortie demandé')
+plt.legend(("NetworkX format PNG", "NetworkX format PDF", "NetworkX format EPS",
+			"NetworkX format SVG"), 'best')
+plt.savefig("execTimeNX.png")
+
+plt.figure(5)
 plt.plot(nbNode, execTimeCoord, 'b')
 plt.xlabel('Nombre de noeuds')
 plt.ylabel('Temps d\'exécution (ms)')
 plt.title('Temps de calcul des coordonnées des noeuds \n en fonction du nombre de noeuds de l\'arbre reçu en entrée')
-plt.show()
+plt.savefig("execTimeCoord.png")
--- Src/toNetworkX.py
+++ Src/toNetworkX.py
@@ -26,30 +26,30 @@ import matplotlib.pyplot as plt
 node_size=5
 
 #Main function
-def toNetworkX(treeToDraw, addLabels=False, outputfile="treeNetworkX.png") :
+def toNetworkX(treeToDraw, addLabels=False, outputfile="treeNetworkX", outputformat="png") :
 	if (not(treeToDraw==None)) :
 		print("Generating NetworkX...")
 		(G, pos) = toNetworkXrec(treeToDraw)
 		print("Printing...")
+		plt.figure(1)
 		plt.gca().invert_yaxis()
-		node_labels = nx.get_node_attributes(G,'label')
-		nx.draw(G, pos, labels=node_labels, with_labels=addLabels, node_size=node_size)
 		# Draw the labels a little upper, so they don't overlap the nodes
 		if (addLabels):
+			node_labels = nx.get_node_attributes(G,'label')
 			pos_labels=dict()
 			for k, v in pos.items():
 				pos_labels[k]=(v[0], v[1]-0.1)
 			nx.draw_networkx_labels(G, pos_labels, labels=node_labels, with_labels=True, node_size=node_size)
-		plt.savefig(outputfile)
+		else :
+			nx.draw(G, pos, with_labels=False, node_size=node_size)
+		print("Saving figure...")
+		plt.savefig(outputfile+'.'+outputformat)
 	else :
 		print("Tree must not be empty.")
 
 #Aux rec function
-def toNetworkXrec(treeToDraw, pos=None) :
-	if (not(pos)) :
-		pos=dict()
+def toNetworkXrec(treeToDraw, pos=dict(), G=nx.Graph()) :
 	#create a graph and add the root and its position
-	G=nx.Graph()
 	G.add_node(hex(id(treeToDraw)), label=treeToDraw.label)
 	pos[hex(id(treeToDraw))] = (treeToDraw.x, treeToDraw.y)
 	#print("noeud courant : ",treeToDraw.label)
@@ -64,9 +64,11 @@ def toNetworkXrec(treeToDraw, pos=None) :
 			#from each son, and combine all the results
 			#G.add_edge(treeToDraw.label, t.label)
 			G.add_edge(hex(id(treeToDraw)), hex(id(t)))
-			(H, pos2) = toNetworkXrec(t, pos)
-			for (node, data) in H.nodes_iter(data=True):
-				G.add_node(node, **data)
-			G.add_edges_from(H.edges())
-			pos.update(pos2)
+			(G, pos) = toNetworkXrec(t, pos, G)
+			#for (node, data) in H.nodes_iter(data=True):
+				#G.add_node(node, **data)
+			#G.add_edges_from(H.edges())
+			#G.add_node(H)
+			#pos.update(pos2)
+			#pos[H]=pos2[hex(id(t))]
 	return (G, pos)
