95f367065aaa7bb248b4e9b69eef5c6aa959c931
Érika Baëna <erika.baena@etu.upmc.fr>
2014-05-09 14:58:26 +0200

    Ajoute la bibliographie.

--- Src/XML_tests/deckGeneration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((i=1; 1000000-$i; i=$(($i+100))))
-do
-	echo "./bin/arbogen -min $i -max $(($i*10)) -type xml -file Deck/tree$i ../Src/Exemples/PSTL_mix.spec"
-	./bin/arbogen -min $i -max $(($i*10)) -type xml -file Deck/tree$i ../Src/Exemples/PSTL_mix.spec >> resArbogen.txt
-done
