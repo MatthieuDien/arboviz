7f82742246dd22bbd948c5f5acd7012101099b34
MDiana <diana.malabard@gmail.com>
2014-05-12 01:05:53 +0200

    Ajout du dossier pour la présentation

--- Src/scriptTestPerf.py
+++ Src/scriptTestPerf.py
@@ -104,8 +104,7 @@ execTimeXMLAsy.append(0)
 execTimeGV=[]
 execTimeGV.append(0)
 
-#nbIte=10
-nbIte=5
+nbIte=10
 nbIteNX=5
 i=1
 n=0
@@ -149,15 +148,15 @@ while i<=33300 and n<=500:
 		a=test.timeit(nbIte)*1000/nbIte
 		execTimeXML.insert(slot, a)
 		
-		## Création du Timer pour le parsing STR
-		#T.toStrFile("XML_tests/test")
-		#test = timeit.Timer("strp()", "from __main__ import strp")
-		#execTimeSTR.insert(slot, test.timeit(nbIte)*1000/nbIte)
+		# Création du Timer pour le parsing STR
+		T.toStrFile("XML_tests/test")
+		test = timeit.Timer("strp()", "from __main__ import strp")
+		execTimeSTR.insert(slot, test.timeit(nbIte)*1000/nbIte)
 		
-		## Création du Timer pour le parsing DOT
+		# Création du Timer pour le parsing DOT
 		T.toDotFile("XML_tests/test")
-		#test = timeit.Timer("dot()", "from __main__ import dot")
-		#execTimeDOT.insert(slot, test.timeit(nbIte)*1000/nbIte)
+		test = timeit.Timer("dot()", "from __main__ import dot")
+		execTimeDOT.insert(slot, test.timeit(nbIte)*1000/nbIte)
 		
 		print ('ComputeCoord pour l\'arbre %d...' % i)
 		
@@ -168,9 +167,9 @@ while i<=33300 and n<=500:
 		
 		print ('Génération de la sortie de l\'arbre %d...' % i)
 		
-		## Création du Timer pour TIKZ
-		#test = timeit.Timer("totikz()", "from __main__ import totikz")
-		#execTimetoTikZ.insert(slot, test.timeit(nbIte)*1000/nbIte)
+		# Création du Timer pour TIKZ
+		test = timeit.Timer("totikz()", "from __main__ import totikz")
+		execTimetoTikZ.insert(slot, test.timeit(nbIte)*1000/nbIte)
 		
 		# Création du Timer pour ASY
 		test = timeit.Timer("toasy()", "from __main__ import toasy")
@@ -179,16 +178,16 @@ while i<=33300 and n<=500:
 		
 		execTimeXMLAsy.insert(slot, a+b+c)
 		
-		#if n<=5000:
-			## Création du Timer pour NX
-			#test = timeit.Timer("tonxpng()", "from __main__ import tonxpng")
-			#execTimetoNXpng.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
-			#test = timeit.Timer("tonxpdf()", "from __main__ import tonxpdf")
-			#execTimetoNXpdf.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
-			#test = timeit.Timer("tonxeps()", "from __main__ import tonxeps")
-			#execTimetoNXeps.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
-			#test = timeit.Timer("tonxsvg()", "from __main__ import tonxsvg")
-			#execTimetoNXsvg.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
+		if n<=5000:
+			# Création du Timer pour NX
+			test = timeit.Timer("tonxpng()", "from __main__ import tonxpng")
+			execTimetoNXpng.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
+			test = timeit.Timer("tonxpdf()", "from __main__ import tonxpdf")
+			execTimetoNXpdf.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
+			test = timeit.Timer("tonxeps()", "from __main__ import tonxeps")
+			execTimetoNXeps.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
+			test = timeit.Timer("tonxsvg()", "from __main__ import tonxsvg")
+			execTimetoNXsvg.insert(slotNX, test.timeit(nbIteNX)*1000/nbIteNX)
 			
 		print ('Exécution de GraphViz pour l\'arbre %d...' % i)
 		
@@ -198,54 +197,54 @@ while i<=33300 and n<=500:
 	if i>=400:
 		i+=500
 	else:
-		i=min(400, i+5)
+		i=min(400, i+100)
 
 # Générer 3 courbes : 1 pour comparer les parsers, une pour computeCoord,
 # une pour les générateurs
 
-#plt.figure(2)
-#plt.loglog()
-#plt.plot(nbNode, execTimeXML, 'b')
-#plt.plot(nbNode, execTimeDOT, 'r')
-#plt.plot(nbNode, execTimeSTR, 'g')
-#plt.xlabel('Nombre de noeuds')
-#plt.ylabel('Temps d\'exécution (ms)')
-#plt.title('Temps d\'exécution des parser \n en fonction du nombre de noeuds de l\'arbre à parser')
-#plt.legend(("Parser XML", "Parser DOT", "Parser STR"), 'best')
-#plt.savefig("execTimeParsers.png")
+plt.figure(2)
+plt.loglog()
+plt.plot(nbNode, execTimeXML, 'b')
+plt.plot(nbNode, execTimeDOT, 'r')
+plt.plot(nbNode, execTimeSTR, 'g')
+plt.xlabel('Nombre de noeuds')
+plt.ylabel('Temps d\'exécution (ms)')
+plt.title('Temps d\'exécution des parser \n en fonction du nombre de noeuds de l\'arbre à parser')
+plt.legend(("Parser XML", "Parser DOT", "Parser STR"), 'best')
+plt.savefig("execTimeParsers.png")
 
-#plt.figure(3)
-#plt.loglog()
-#plt.plot(nbNode, execTimetoAsy, 'b')
-#plt.plot(nbNode, execTimetoTikZ, 'g')
-#plt.xlabel('Nombre de noeuds')
-#plt.ylabel('Temps d\'exécution (ms)')
-#plt.title('Temps d\'exécution des générateurs Asymptote et TikZ \n'+
-		  #' en fonction du nombre de noeuds de l\'arbre à afficher')
-#plt.legend(("Asymptote", "TikZ"), 'best')
-#plt.savefig("execTimeGenerators.png")
+plt.figure(3)
+plt.loglog()
+plt.plot(nbNode, execTimetoAsy, 'b')
+plt.plot(nbNode, execTimetoTikZ, 'g')
+plt.xlabel('Nombre de noeuds')
+plt.ylabel('Temps d\'exécution (ms)')
+plt.title('Temps d\'exécution des générateurs Asymptote et TikZ \n'+
+		  ' en fonction du nombre de noeuds de l\'arbre à afficher')
+plt.legend(("Asymptote", "TikZ"), 'best')
+plt.savefig("execTimeGenerators.png")
 
-#plt.figure(4)
-#plt.plot(nbNodeNX, execTimetoNXpng, 'r')
-#plt.plot(nbNodeNX, execTimetoNXpdf, 'g')
-#plt.plot(nbNodeNX, execTimetoNXeps, 'b')
-#plt.plot(nbNodeNX, execTimetoNXsvg, 'm')
-#plt.xlabel('Nombre de noeuds')
-#plt.ylabel('Temps d\'exécution (ms)')
-#plt.title('Temps d\'exécution du générateur NetworkX + Pyplot en fonction du \n'+
-		  #'nombre de noeuds de l\'arbre à afficher et du type de sortie demandé')
-#plt.legend(("NetworkX format PNG", "NetworkX format PDF", "NetworkX format EPS",
-			#"NetworkX format SVG"), 'best')
-#plt.savefig("execTimeNX.png")
+plt.figure(4)
+plt.plot(nbNodeNX, execTimetoNXpng, 'r')
+plt.plot(nbNodeNX, execTimetoNXpdf, 'g')
+plt.plot(nbNodeNX, execTimetoNXeps, 'b')
+plt.plot(nbNodeNX, execTimetoNXsvg, 'm')
+plt.xlabel('Nombre de noeuds')
+plt.ylabel('Temps d\'exécution (ms)')
+plt.title('Temps d\'exécution du générateur NetworkX + Pyplot en fonction du \n'+
+		  'nombre de noeuds de l\'arbre à afficher et du type de sortie demandé')
+plt.legend(("NetworkX format PNG", "NetworkX format PDF", "NetworkX format EPS",
+			"NetworkX format SVG"), 'best')
+plt.savefig("execTimeNX.png")
 
-#plt.figure(5)
-#plt.loglog()
-#plt.plot(nbNode, execTimeCoord, 'b')
-#plt.xlabel('Nombre de noeuds')
-#plt.ylabel('Temps d\'exécution (ms)')
-#plt.title('Temps de calcul des coordonnées des noeuds \n'+
-		  #' en fonction du nombre de noeuds de l\'arbre reçu en entrée')
-#plt.savefig("execTimeCoord.png")
+plt.figure(5)
+plt.loglog()
+plt.plot(nbNode, execTimeCoord, 'b')
+plt.xlabel('Nombre de noeuds')
+plt.ylabel('Temps d\'exécution (ms)')
+plt.title('Temps de calcul des coordonnées des noeuds \n'+
+		  ' en fonction du nombre de noeuds de l\'arbre reçu en entrée')
+plt.savefig("execTimeCoord.png")
 
 plt.figure(6)
 plt.plot(nbNodeNX, execTimeGV, 'r')
