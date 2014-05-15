4c288c6001c56066b90424db16ee172178bb840b
MDiana <diana.malabard@gmail.com>
2014-05-15 12:02:16 +0200

    figure graphViz sans labels mise à jour ajout du protocole de benchmark

--- Src/test.py
+++ Src/test.py
@@ -17,8 +17,6 @@ This file is part of TreeDisplay.
     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import argparse
-
 from tree import *
 
 #from arbParser import *
@@ -26,69 +24,7 @@ from dotParser import *
 from strParser import *
 from xmlParser import *
 
-from toNetworkX import *
-from toTikZ import *
-from toAsymptote import *
-
-#*****************
-#*** Arguments ***
-#*****************
-
-use = argparse.ArgumentParser()
-
-use.add_argument("ext", choices=["str", "arb", "xml", "dot"], help="The type of the given file.")
-use.add_argument("src", help="The file which contains a tree description.")
-use.add_argument("-L", "--labels", action="store_true", help="Print labels on the output tree, depreciate with NetworkX and Asymptote")
-use.add_argument("-O", "--output", default="png", choices=["tikz", "asy", "png", "pdf", "eps"], help="The type of the output file")
-use.add_argument("-N", "--name", default="result", help="The name of the output file")
-
-args = use.parse_args()
-
-ext = args.ext
-src = args.src
-withLabels = args.labels
-output = args.output
-fileName = args.name
-
-#***************
-#*** Parsing ***
-#***************
-
-print("Parsing...")
-
-if (ext == "dot"):
-	tree = dotParser (src)
-elif (ext == "str"):
-	tree = strParser (src)
-elif (ext == "xml"):
-	tree = xmlParser (src)
-elif (ext == "arb"):
-	#tree = arbParser (src)
-	raise Exception ("Temporairement non pris en charge")
-else:
-	raise Exception ("Temporairement non pris en charge")
-
-#*****************
-#*** Computing ***
-#*****************
-
-print("Computing...")
-
-tree.toDotFile()
-#tree.computeCoord()
-#tree.setup()
-
-#******************
-#*** Generating ***
-#******************
-
-"""
-print("Generating...")
-
-if (output == "tikz"):
-	toTikz (tree, withLabels, fileName + ".tex")
-elif (output == "asy"):
-	toAsymptote (tree, withLabels, fileName + ".tex")
-elif (output == "png" or output == "pdf" or output == "eps"):
-	toNetworkX (tree, withLabels, fileName + "." + output)
-"""
+#xmlParser('XML_tests/treeXml30100To33300/tree33300.xml').toDotFile('huge')
+#xmlParser('XML_tests/treeXml30100To33300/tree33300.xml').toStrFile('huge')
+dotParser('huge.dot')
+#strParser('huge.txt')
