54d9b0773ae2bfe177f7d31b283607154ba740a5
matthieu <matthieu.dien@gmail.com>
2014-09-11 17:58:20 +0200

    revert tree's pictures

--- src/backends/dot/DotBackend.ml
+++ src/backends/dot/DotBackend.ml
@@ -6,12 +6,12 @@ module DotBackend : Backend =
   struct
     let name = "Dot Backend"
 
-    let string_of_pos pos width_unit height_unit =
+    let string_of_pos _ height pos width_unit height_unit =
       sprintf "pos = \"%f,%f!\""
         (pos.x *. width_unit)
-        ((float_of_int pos.y) *. height_unit)
+        (height -. (float_of_int pos.y) *. height_unit)
 
-    let write_nodes outchan tree width_unit height_unit show_label =
+    let write_nodes outchan tree width height width_unit height_unit show_label =
       let current_id = 0 in
       let f =  fun current_id (name, _, pos) ->
        begin
@@ -20,7 +20,7 @@ module DotBackend : Backend =
              output_string outchan ("label=\"" ^ name ^ "\"")
           else
              output_string outchan ("shape=\"point\""));
-         output_string outchan ((string_of_pos pos width_unit height_unit) ^ "\n]\n");
+         output_string outchan ((string_of_pos width height pos width_unit height_unit) ^ "\n]\n");
          (* output_string outchan ("\n]\n"); *)
          current_id + 1
        end
@@ -52,10 +52,10 @@ module DotBackend : Backend =
       in
       ignore (Tree.prefix_fold first_sons f state)
       
-    let write outchan tree _ _ width_unit height_unit show_label =
+    let write outchan tree width height width_unit height_unit show_label =
       output_string outchan "graph G {\n";
       output_string outchan "graph [ordering=\"out\"]\n";
-      write_nodes outchan tree width_unit height_unit show_label;
+      write_nodes outchan tree width (float_of_int height) width_unit height_unit show_label;
       (match tree with
       | Node (_,[],_) -> ()
       | Node (_,childs,_) -> write_edges outchan childs);
